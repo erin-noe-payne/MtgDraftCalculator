76c103983d4f8b88c166feac5c86c77b4c802d59
autoric <saizul@gmail.com>
2011-11-11 12:13:19 -0500

    update index

--- client/js/index.js
+++ client/js/index.js
@@ -5,25 +5,46 @@
 
 $(function() {
     
+    /*
+     * Event Listeners
+     */
     $('.last').live('keypress', function(){
-        $(this).removeClass('last').addClass('name');
+        addLi($(this));
+    })
+    
+    $('.name').live('blur', function() {
+        if($(this).find('input').val() == '') {
+            removeLi($(this));
+        }
+    })
+    
+    $('.deleteButton').live('click', function() {
+        var li = $(this).closest('li');
+        removeLi(li);
+    });
+    
+    /*
+     * Functionality
+     */
+    function addLi(li){
+        $(li).removeClass('last').addClass('name');
+        $(li).append($('<button type="button" class="deleteButton">X</button>'))
+        
         var $listItem = $('<li class="last"><input class="name"></input></li>').hide();
         $('#nameList').append($listItem);
         $listItem.animate({
             opacity: 'toggle', 
             height: 'toggle'
         }, 'fast');
-    })
+    }
     
-    $('.name').live('blur', function() {
-        if($(this).find('input').val() == '') {
-            $(this).animate({
-                opacity: 'toggle', 
-                height: 'toggle'
-            }, 'fast', function(){
-                $(this).remove();
-            })
-        }
-    })
+    function removeLi(li) {
+        $(li).animate({
+            opacity: 'toggle', 
+            height: 'toggle'
+        }, 'fast', function(){
+            $(li).remove();
+        })
+    }
 })
 
